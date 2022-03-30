c8424e3fd696152bdbb85345f867c535cd3ca3ef
Aitor <aitor.ventura00@gmail.com>
2022-03-30 23:42:45 +0100

    envinronments vairable changed to support firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDM_vV_ksnOoM-TzfP9i9Qo4MvX77KknZ0",
+    authDomain: "daw2-96f00.firebaseapp.com",
+    databaseURL: "https://daw2-96f00-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "daw2-96f00",
+    storageBucket: "daw2-96f00.appspot.com",
+    messagingSenderId: "646564479004",
+    appId: "1:646564479004:web:25bc06b574d91b346a9fd9"
+  }
 };
